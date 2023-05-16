0e73176c058abe5c7840fe72d9be9c0814042ef4
Mete Tatar <mete-tatar@hotmail.com>
2023-05-16 14:26:23 +0300

    first commit

---
+++ degisken.js
@@ -0,0 +1,53 @@
+// Hello world yazdıma
+
+/*
+
+console.log("hello world");
+
+
+// Let kullanımı
+let isim = "Mete";
+
+let soyisim = "Tatar";
+
+let nick = "Yotta";
+console.log(nick,soyisim,isim)
+
+*/
+
+// let,var,const Kullanım
+
+/*
+
+var Isim = "Mete";
+console.log("Girilen kişinin ismi =",Isim)
+
+let soyi = "Tatar";
+console.log("Girilen kişinin Soyismi =",soyi)
+
+const tc = 123124464;
+console.log("Girilen kişinin Tc'si =",tc)
+
+
+
+var Isim = "Emirhan";
+soyi = "Ulusoy";
+
+top = Isim + soyi + tc
+
+console.log("Değişen Verilerin Toplamı = ",top)
+
+*/
+
+// vize ve final ort
+
+/*
+
+let vize = 60
+let final = 100
+
+let ort = (vize * 0.4) + (final * 0.6)
+
+console.log("ort =",ort)
+
+*/
