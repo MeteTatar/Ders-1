828c72685b7b1eb9eeb60ed5a27c0554956e83bf
Mete Tatar <mete-tatar@hotmail.com>
2023-05-17 17:07:44 +0300

    Update

---
+++ 04 - degisken_kontrol_ve_degistirme.js
@@ -0,0 +1,9 @@
+let numara = 123;
+let yazinum = "123";
+let Password = true;
+
+console.log("Numara = ", typeof(numara));
+
+console.log("Yazinum = ", typeof(yazinum));
+
+console.log("Password = ", typeof(Password));
