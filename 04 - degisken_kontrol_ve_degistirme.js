732acd5a0d0d81dfc480ccf5f4a9140f34b57df7
Mete Tatar <mete-tatar@hotmail.com>
2023-05-17 17:30:58 +0300

    Update

--- 04 - degisken_kontrol_ve_degistirme.js
+++ 04 - degisken_kontrol_ve_degistirme.js
@@ -9,3 +9,21 @@ console.log("Numara = ", typeof(numara));
 console.log("Yazinum = ", typeof(yazinum));
 
 console.log("Password = ", typeof(Password));
+
+// String'i int'e dönüştürme
+
+no = "150";
+no = parseInt(no);
+console.log("No 1 = ",no , "\nTürü = ", typeof(no));
+
+no2 = "256px"
+no2 = parseInt(no2);
+console.log("No 2 = ", no2 , "\nTürü = ", typeof(no2));
+
+no3 = "256px"
+no3 = Number(no3);
+console.log("No 3 = ", no3 , "\nTürü = ", typeof(no3)); // number sadece sayısal değerleri dönüştürmek için, yanında başka hiçbir karakter harf olmamalı
+
+no4 = "356px"
+no4 = parseFloat(no4);
+console.log("No 4 = ", no4 , "\nTürü = ", typeof(no4));
