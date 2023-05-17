5d4275938db12790e1648b86f864e2fb39165b1f
Mete Tatar <mete-tatar@hotmail.com>
2023-05-17 16:51:16 +0300

    Update

--- 03 - boolean_veri_tip.js
+++ 03 - boolean_veri_tip.js
@@ -5,5 +5,13 @@ isActive = true // 1 yani burada isActive değişkenini doğruladık
 
 console.log(isActive)
 
-Boolean("11") // true
+Boolean("11") // true içerisinde bilgi var
+Boolean("0") // true içerisinde bilgi var
+Boolean("") // false İçerisinde bilgi yok ise false
 
+// 0, -0, 0.1
+
+Boolean(0) // false
+Boolean(-0) // false
+Boolean(-0,1) // true
+Boolean(0 === 0) // true
