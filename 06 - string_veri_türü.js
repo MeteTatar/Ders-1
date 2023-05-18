487436de1ff42438962624300867486acf1b30fe
Mete Tatar <mete-tatar@hotmail.com>
2023-05-18 15:06:46 +0300

    Update

---
+++ 06 - string_veri_türü.js
@@ -0,0 +1,14 @@
+let fname = "Mete";
+let sname = "Tatar";
+const tc = 3255435346;
+
+
+// string karakter uzunluğu
+
+console.log(fname.length);
+
+// belirli karakteri bulma
+
+console.log(sname[4])
+console.log(sname.charAt(2))
+
