bd842457cb693f21cfc59426bfb61991f7d028e6
Mete Tatar <mete-tatar@hotmail.com>
2023-05-18 09:12:09 +0300

    Update

--- 05 - tamplate_literals.js
+++ 05 - tamplate_literals.js
@@ -1,3 +1,16 @@
 let username = "Mete";
 let surname = "Tatar";
-let domain = 
+const domain = "hotmail.com";
+
+let email = username + "-" + surname + "@" + domain;
+
+let literals = 
+`
+Merhaba ${username} Sitemize hoşgeldin...
+e-mail adresin = ${email}
+e-mail adresinin uzunluğu = ${email.length}
+Borcunuz = ${(6+9)*6} ₺
+Saat = ${new Date().getHours()}
+`  
+
+console.log(literals);
