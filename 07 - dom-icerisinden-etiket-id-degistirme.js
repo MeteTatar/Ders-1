b7a46e9926db3ba2c39f3633f15a6b9326aa8fbc
Mete Tatar <mete-tatar@hotmail.com>
2023-05-19 17:43:59 +0300

    Update

--- 07 - dom-icerisinden-etiket-id-degistirme.js
+++ 07 - dom-icerisinden-etiket-id-degistirme.js
@@ -3,3 +3,8 @@
 let title = document.getElementById('title')
 title.innerHTML = "dom içi bilgi değiştirme"
 console.log(title.innerHTML)
+
+let link = document.querySelector("#kodluyoruzLink")
+link.innerHTML += " değişti"
+link.style.color = "red"
+link.classList.add("btn")
