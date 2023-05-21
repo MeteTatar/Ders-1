197ecb813fb5a502e275122d79cb596848b31aa7
Mete Tatar <mete-tatar@hotmail.com>
2023-05-21 20:27:05 +0300

    Update

--- 08 - list-icinde-oge-erismek-ve-yeni-oge-ekleme.js
+++ 08 - list-icinde-oge-erismek-ve-yeni-oge-ekleme.js
@@ -2,3 +2,9 @@
 
 let lastChild = document.querySelector("ul#list>li:last-child")
 lastChild.innerHTML = "Son öğe değişti..."
+
+let firstChild = document.querySelector("ul#list>li:first-child")
+firstChild.innerHTML = "İlk öğe değişti..."
+
+
+
