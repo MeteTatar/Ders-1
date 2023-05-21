90495ee593ffdebd24c2aa966491916a49beebc2
Mete Tatar <mete-tatar@hotmail.com>
2023-05-21 20:48:51 +0300

    Update

--- 08 - list-icinde-oge-erismek-ve-yeni-oge-ekleme.js
+++ 08 - list-icinde-oge-erismek-ve-yeni-oge-ekleme.js
@@ -6,5 +6,12 @@ lastChild.innerHTML = "Son öğe değişti..."
 let firstChild = document.querySelector("ul#list>li:first-child")
 firstChild.innerHTML = "İlk öğe değişti..."
 
+let ulDOM = document.querySelector("ul#list")
+let liDOM = document.createElement("li")
+
+liDOM.innerHTML = "Kendi oluşturduğumuz öğe..."
+
+// ulDOM.append(liDOM) // en sona ekleme
+ulDOM.prepend(liDOM) // ilk başa ekleme
 
 
