50d1b0ba077a61b66c8d1fbbb70c9b707d5f9387
Mete Tatar <mete-tatar@hotmail.com>
2023-05-22 08:10:58 +0300

    Update

--- 09 - Dom-ile-class-ekleme-veya-çıkarma.js
+++ 09 - Dom-ile-class-ekleme-veya-çıkarma.js
@@ -1,1 +1,10 @@
 // dom ile class bilgisi ekleme veya çıkarma
+
+let jstyle = document.querySelector("#jstyle")
+
+jstyle.classList.add("text-green")
+jstyle.classList.add("title" , "text-blue" , "swm" , "rvb") // birden fazla class ekleme
+
+jstyle.classList.remove("title") 
+jstyle.classList.remove("text-blue" , "swm" , "rvb") // birden fazla class silme
+
